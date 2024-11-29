69b0f07abfef3a592d33629810570080062df5e7
uninh <uninh@student.kit.edu>
2024-11-29 15:35:08 +0000

    ordnerstruktur geändert

--- converter/converted.py
+++ converter/converted.py
@@ -1,41 +1,37 @@
-import rospy
+##### DO NOT CHANGE #####
+
 import sys
+import math
+
+import rospy
 from moveit_commander import MoveGroupCommander, roscpp_initialize, roscpp_shutdown
 
-"""
-Ziel :
-Den Status des Roboters abfragen und interpretieren.
-Diese Aufgabe soll helfen, den aktuellen Zustand des
-Roboters (Positionen, Orientierungen, Gelenkwinkel) zu überprüfen
-und zu verstehen, wie er während oder nach einer Bewegung auf Feedback zugreifen kann.
-"""
+##### DO NOT CHANGE #####
 
-# intialize the MoveIt! commander from the moveit_command package
+##### DO NOT CHANGE #####
+
 roscpp_initialize(sys.argv)
+rospy.init_node('exercise_1_1', anonymous=True)
 
-# Initialisiere den ROS-Knoten
-rospy.init_node('exercise_4_1', anonymous=True)
+##### DO NOT CHANGE #####
 
-# Initialisiere die MoveGroup
-group = MoveGroupCommander('panda_arm')
+##### DO NOT CHANGE #####
 
-# set the planner ID to 'RRTConnectkConfigDefault'
+group = MoveGroupCommander("panda_arm")
 group.set_planner_id("RRTConnectkConfigDefault")
-
-# set the number of planning attempts to 10
 group.set_num_planning_attempts(10)
 
-# Abfrage der aktuellen Gelenkwinkel
-joint_values = group.get_current_joint_values()
-rospy.loginfo("Current Joint Values: ")
-for i, joint in enumerate(joint_values):
-    rospy.loginfo(f"Joint {i+1}: {joint:.2f} rad")
+##### DO NOT CHANGE #####
 
-# Abfrage der aktuellen Endeffektor-Position
-pose = group.get_current_pose().pose
-rospy.loginfo(f"Endeffector Position: x={pose.position.x:.3f}, y={pose.position.y:.3f}, z={pose.position.z:.3f}")
-rospy.loginfo(f"Endeffector Orientation: x={pose.orientation.x:.3f}, y={pose.orientation.y:.3f}, z={pose.orientation.z:.3f}, w={pose.orientation.w:.3f}")
+joint_goal = group.get_current_joint_values()
+joint_goal[4] = (155 * math.pi) / 180
 
-# delete the ros node
+group.set_joint_value_target(joint_goal)
+group.go(wait=True)
+
+##### DO NOT CHANGE #####
+
 rospy.signal_shutdown("Task completed")
 roscpp_shutdown()
+
+##### DO NOT CHANGE #####
