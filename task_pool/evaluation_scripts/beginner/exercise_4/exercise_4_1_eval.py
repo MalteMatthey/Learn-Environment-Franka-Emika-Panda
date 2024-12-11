a162458eec7ed70202b159690fa24f6bbc08b3cc
uninh <uninh@student.kit.edu>
2024-12-11 12:13:07 +0000

    exercise_4_1, exercise_4_2, exercise_5_1, exercise_5_2 translated and eval skript added and added exercise 5_3

--- task_pool/evaluation_scripts/beginner/exercise_4/exercise_4_1_eval.py
+++ task_pool/evaluation_scripts/beginner/exercise_4/exercise_4_1_eval.py
@@ -1,1 +1,39 @@
-print("TODO: WRITE EVALUATION SCRIPT")
+import sys
+import math
+
+import rospy
+from moveit_commander import MoveGroupCommander, roscpp_initialize, roscpp_shutdown
+
+
+rospy.loginfo("exercise_4_1_evaluation")
+
+roscpp_initialize(sys.argv)
+rospy.init_node('exercise_4_1_evaluation', anonymous=True)
+
+group = MoveGroupCommander("panda_arm")
+
+current_pose = group.get_current_pose().pose
+expected_pose = [0.1, 0.1, 0.1, 0.0, 0.0, 0.0, 1.0]
+error_margin = 0.01
+
+current_values = [
+    current_pose.position.x,
+    current_pose.position.y,
+    current_pose.position.z,
+    current_pose.orientation.x,
+    current_pose.orientation.y,
+    current_pose.orientation.z,
+    current_pose.orientation.w
+]
+
+for i, (current, expected) in enumerate(zip(current_values, expected_pose)):
+    if abs(current - expected) > error_margin:
+        
+        roscpp_shutdown()
+
+        print("false, Pose component {} is not in the correct position".format(i+1))
+        sys.exit(0)
+
+roscpp_shutdown()
+
+print("true")
--- task_pool/evaluation_scripts/beginner/exercise_5/exercise_5_2_eval.py
+++ task_pool/evaluation_scripts/beginner/exercise_5/exercise_5_2_eval.py
@@ -1,1 +1,34 @@
-print("TODO: WRITE EVALUATION SCRIPT")
+import rospy
+from moveit_commander import PlanningSceneInterface, roscpp_initialize, roscpp_shutdown
+
+def evaluate_scene_empty():
+    """
+    Checks whether all objects have been removed from the planning environment.
+    Issues a success message if there are no objects in the scene,
+    Otherwise, a warning.
+    """
+    # Initialize ROS and PlanningSceneInterface
+    roscpp_initialize([])
+    rospy.init_node('evaluate_scene_empty', anonymous=True)
+
+    # Interface for PlanningScene
+    scene = PlanningSceneInterface()
+
+    # Short delay to make sure the scene is loaded
+    rospy.sleep(1)
+
+    # Query the currently existing objects in the scene
+    current_objects = scene.get_known_object_names()
+
+    if not current_objects:
+        rospy.loginfo("The scene is empty. All objects have been successfully removed.")
+    else:
+        rospy.logwarn("The scene is not empty. The following objects are still there:")
+        for obj in current_objects:
+            rospy.logwarn(f" - {obj}")
+
+    # Shutdown ROS
+    roscpp_shutdown()
+
+if __name__ == "__main__":
+    evaluate_scene_empty()
