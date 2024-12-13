1742ee02c5fd5f9b04c43778b71499b34693dfb7
Max <uqwfa@student.kit.edu>
2024-12-13 12:53:42 +0100

    add move_it_basics task 1

---
+++ task_pool/evaluation_scripts/move_it/move_it_basics/move_it_basics_1_eval.py
@@ -0,0 +1,30 @@
+import rospy
+import math
+import sys
+
+from moveit_commander import MoveGroupCommander, roscpp_initialize, roscpp_shutdown
+
+
+roscpp_initialize(sys.argv)
+rospy.init_node('move_it_basics_1_eval', anonymous=True)
+
+group = MoveGroupCommander("panda_arm")
+group.set_planner_id("RRTConnectkConfigDefault")
+group.set_num_planning_attempts(10)
+
+current_joint_values = group.get_current_joint_values()
+
+expected_values = [0, -math.pi/4, 0, -math.pi *3/4, math.pi * 50/180, math.pi/2, math.pi/4]
+
+error_margin = 0.01
+
+for i, (current, expected) in enumerate(zip(current_joint_values, expected_values)):
+    if abs(current - expected) > error_margin:
+        
+        roscpp_shutdown()
+        print("false, Joint {} is not in the correct position".format(i))
+        sys.exit(0)
+
+roscpp_shutdown()
+
+print("true")
